713ded37fc86ed7154a461570d951868e96b12d5
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-04-17 17:04:46 +0300

    feat: done some tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,20 +6,23 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity.length === 0) return false;
+  if (!Number(sampleActivity)) return false;
+  if (Number(sampleActivity) > 15 || Number(sampleActivity) < 0) return false;
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) * HALF_LIFE_PERIOD / 0.693);
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -3,21 +3,51 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!')
+  }
+
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--discard-prev') {
+      if (newArr[newArr.length - 1] === arr[i - 1] && arr[i - 1]) {
+        newArr.pop();
+      }
+    } else if (arr[i] === '--double-next') {
+      if (arr[i + 1]) {
+        newArr.push(arr[i + 1]);
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (newArr[newArr.length - 1] === arr[i - 1] && arr[i - 1]) {
+        newArr.push(arr[i - 1]);
+      }
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+
+  return newArr;
 }
 
 module.exports = {
   transform
 };
+
+let arr = []
+let i = arr.pop()
+console.log(i)
