1d725849e741851f32ff3a9609d71322c22a85be
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-04-18 01:23:22 +0300

    feat: done some tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,22 +2,31 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  options.repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+  options.separator = options.separator ? options.separator : '+';
+  options.additionRepeatTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+  options.additionSeparator = options.additionSeparator ? options.additionSeparator : '|';
+
+  let add = '';
+  if (Object.getOwnPropertyNames(options).includes('addition')) {
+    add = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1) + options.addition;
+  }
+  let result = (str + add + options.separator).repeat(options.repeatTimes - 1) + str + add;
+  return result;
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,9 +3,9 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr, counter = 1) {
+    let depth = counter;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        const recursionDepth = this.calculateDepth(arr[i], counter + 1);
+        depth = depth > recursionDepth ? depth : recursionDepth;
+      }
+    }
+    return depth;
   }
 }
 
